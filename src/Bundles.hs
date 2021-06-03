228dac2ab46503d96ab8c3b3811212f244f48db0
Gustavo Bicalho <gusbicalho@gmail.com>
2021-06-03 04:57:58 +0000

    add BundlingSetup

--- src/Bundles.hs
+++ src/Bundles.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE RankNTypes #-}
@@ -17,6 +18,7 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE UndecidableSuperClasses #-}
 {-# OPTIONS_GHC -Wno-missing-export-lists #-}
+{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
 
 {- |
 Copyright: (c) 2021 Gustavo Bicalho
@@ -101,36 +103,6 @@ instance
   getExport (Exporting _ bundle) = getExport @exportName @export bundle
   getExport (Skipping bundle) = getExport @exportName @export bundle
 
-type MorphBundle :: TypeMap -> TypeMap -> Constraint
-class MorphBundle fromExports toExports where
-  morphBundle :: Bundle name owner fromExports -> Bundle name owner toExports
-
-instance MorphBundle fromExports '[] where
-  morphBundle (_ :: Bundle name owner fromExports) = Bundle @name @owner
-
-instance MorphBundle exports exports where
-  morphBundle bundle = bundle
-
-instance
-  ( MorphBundle fromExports toMoreExports
-  , forall name owner. GetExport exportName export (Bundle name owner fromExports)
-  ) =>
-  MorphBundle fromExports (exportName ':-> export ': toMoreExports)
-  where
-  morphBundle bundle =
-    case getExport @exportName bundle of
-      Just export -> base & Exporting @exportName export
-      Nothing -> base & Skipping @exportName
-   where
-    base = morphBundle @fromExports @toMoreExports bundle
-
-morph ::
-  forall toExports fromExports name owner.
-  (MorphBundle fromExports toExports) =>
-  Bundle name owner fromExports ->
-  Bundle name owner toExports
-morph = morphBundle @fromExports @toExports
-
 type SomeBundle :: TypeMap -> Type
 data SomeBundle requiredExports where
   SomeBundle ::
@@ -141,11 +113,59 @@ data SomeBundle requiredExports where
     Bundle name owner exports ->
     SomeBundle exports
 
+type MorphBundle :: (TypeMap -> Type) -> TypeMap -> TypeMap -> Constraint
+class MorphBundle bundleC fromExports toExports where
+  morphBundle :: bundleC fromExports -> bundleC toExports
+
+instance MorphBundle (Bundle name owner) fromExports '[] where
+  {-# INLINE morphBundle #-}
+  morphBundle _ = Bundle @name @owner
+
+instance
+  ( MorphBundle (Bundle name owner) fromExports toMoreExports
+  , GetExport exportName export (Bundle name owner fromExports)
+  ) =>
+  MorphBundle (Bundle name owner) fromExports (exportName ':-> export ': toMoreExports)
+  where
+  {-# INLINE morphBundle #-}
+  morphBundle bundle =
+    case getExport @exportName bundle of
+      Just export -> base & Exporting @exportName export
+      Nothing -> base & Skipping @exportName
+   where
+    base = morphBundle @_ @fromExports @toMoreExports bundle
+
+instance MorphBundle SomeBundle fromExports '[] where
+  {-# INLINE morphBundle #-}
+  morphBundle (SomeBundle (_ :: Bundle name owner exports)) = SomeBundle $ Bundle @name @owner
+
+instance
+  ( forall name owner. MorphBundle (Bundle name owner) fromExports toMoreExports
+  , forall name owner. GetExport exportName export (Bundle name owner fromExports)
+  ) =>
+  MorphBundle SomeBundle fromExports (exportName ':-> export ': toMoreExports)
+  where
+  {-# INLINE morphBundle #-}
+  morphBundle (SomeBundle bundle) =
+    case morphBundle @_ @fromExports @toMoreExports bundle of
+      base ->
+        case getExport @exportName @export bundle of
+          Just export -> SomeBundle $ base & Exporting @exportName export
+          Nothing -> SomeBundle $ base & Skipping @exportName
+
+{-# INLINE morph #-}
+morph ::
+  forall toExports fromExports bundleC.
+  (MorphBundle bundleC fromExports toExports) =>
+  bundleC fromExports ->
+  bundleC toExports
+morph = morphBundle @_ @fromExports @toExports
+
 someBundle ::
   forall exports name owner bundleExports.
   ( KnownSymbol name
   , KnownSymbol owner
-  , MorphBundle bundleExports exports
+  , MorphBundle (Bundle name owner) bundleExports exports
   ) =>
   Bundle name owner bundleExports ->
   SomeBundle exports
@@ -157,6 +177,49 @@ data BundleFactory name owner imports exports where
     forall name owner imports exports.
     ([SomeBundle imports] -> [SomeBundle exports]) ->
     BundleFactory name owner imports exports
+
+data BundleFactorySig where
+  BundleFactorySig :: Symbol -> Symbol -> TypeMap -> TypeMap -> BundleFactorySig
+
+data BundleSig where
+  BundleSig :: Symbol -> Symbol -> TypeMap -> BundleSig
+
+type Bundles :: [BundleSig] -> Type
+data Bundles signatures where
+  NoBundles :: Bundles '[]
+  AddBundle ::
+    Bundle name owner exports ->
+    Bundles sigs ->
+    Bundles ( 'BundleSig name owner exports : sigs)
+
+type Factories :: [BundleFactorySig] -> Type
+data Factories signatures where
+  NoFactories :: Factories '[]
+  AddFactory ::
+    BundleFactory name owner imports exports ->
+    Factories sigs ->
+    Factories ( 'BundleFactorySig name owner imports exports : sigs)
+
+type BundlingSetup :: [BundleSig] -> [BundleFactorySig] -> Type
+data BundlingSetup bundles factories where
+  BundlingSetup :: Bundles bundles -> Factories factories -> BundlingSetup bundles factories
+
+emptySetup :: BundlingSetup '[] '[]
+emptySetup = BundlingSetup NoBundles NoFactories
+
+addBundle ::
+  Bundle name owner exports ->
+  BundlingSetup bundles factories ->
+  BundlingSetup ( 'BundleSig name owner exports : bundles) factories
+addBundle bundle (BundlingSetup bundles factories) =
+  BundlingSetup (bundles & AddBundle bundle) factories
+
+addFactory ::
+  BundleFactory name owner imports exports ->
+  BundlingSetup bundles factories ->
+  BundlingSetup bundles ( 'BundleFactorySig name owner imports exports : factories)
+addFactory factory (BundlingSetup bundles factories) =
+  BundlingSetup bundles (factories & AddFactory factory)
 
 bundleA ::
   Bundle
@@ -186,4 +249,8 @@ factoryFoo = BundleFactory $ \inputBundles ->
 runFactory :: BundleFactory name owner imports exports -> [SomeBundle imports] -> [SomeBundle exports]
 runFactory (BundleFactory runF) = runF
 
--- b = getExport @"foo/val" @Int a
+assembled :: BundlingSetup _ _
+assembled =
+  emptySetup
+    & addBundle bundleA
+      . addFactory factoryFoo
