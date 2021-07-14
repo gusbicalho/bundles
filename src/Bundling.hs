b0091592aa9ea68430f4d52ef97aef2f0c3a3aa2
Gustavo Bicalho <gusbicalho@gmail.com>
2021-07-14 09:23:25 +0000

    wip

--- src/Bundling.hs
+++ src/Bundling.hs
@@ -4,7 +4,9 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE PolyKinds #-}
@@ -29,54 +31,97 @@ See README for more info
 -}
 module Bundling where
 
-import Bundling.Bundle (
-  Bundle (Bundle, Exporting),
-  getExport,
-  SomeBundle (..),
-  someBundle, getExportMaybe
- )
-import Bundling.BundlingSetup (
-  BundlingSetup,
-  addBundle,
-  addFactory,
-  emptySetup,
- )
-import Bundling.Factory (BundleFactory (..))
-import Bundling.TypeMap (KMapEntry ((:->)))
-import Data.Function ((&))
-import qualified Data.Maybe as Maybe
-import Data.Text (Text)
+import Bundling.TypeSet (TypeSet)
+import Bundling.TypeSet qualified as TS
+import Control.Arrow ((&&&))
+import Data.Foldable qualified as Foldable
+import Data.Kind (Constraint, Type)
+import Data.Typeable (Proxy (Proxy), TypeRep, Typeable, typeRep)
+import GHC.Base (Any)
+import GHC.TypeLits (ErrorMessage (..), KnownSymbol, Symbol, TypeError, symbolVal)
+import HList (HList (HNil, (:::)))
+import Unsafe.Coerce (unsafeCoerce)
+import Numeric.Natural (Natural)
 
-bundleA ::
-  Bundle
-    "bundleA"
-    "red"
-    '["bar/name" ':-> Text, "foo/val" ':-> Word]
-bundleA =
-  Bundle @"bundleA" @"red"
-    & Exporting @"foo/val" @Word 42
-    & Exporting @"bar/name" @Text "Pretty foolish"
+type BundleMeta = String
+type RepMap = [(TypeRep, Any)]
 
-factoryFoo ::
-  BundleFactory
-    "factoryFoo"
-    "blue"
-    '["foo/val" ':-> Word, "foo/name" ':-> Text]
-    '["bar/total" ':-> Word, "bar/names" ':-> [Text]]
-factoryFoo = BundleFactory $ \inputBundles ->
-  let total = inputBundles & Maybe.mapMaybe (getExportMaybe @"foo/val" @Word) & sum
-      names = inputBundles & Maybe.mapMaybe (getExportMaybe @"foo/name" @Text)
-   in [ someBundle $
-          Bundle @"factoryFoo.bundle" @"blue"
-            & Exporting @"bar/total" total
-            & Exporting @"bar/names" names
-      ]
+lookupT :: forall t. (Typeable t) => RepMap -> Maybe t
+lookupT repMap =
+  case lookup (typeRep (Proxy @t)) repMap of
+    Nothing -> Nothing
+    Just a -> Just (unsafeCoerce a :: t)
 
-runFactory :: BundleFactory name owner imports exports -> [SomeBundle] -> [SomeBundle]
-runFactory (BundleFactory runF) = runF
+data Bundle = Bundle
+  { bundleMeta :: BundleMeta
+  , bundleExports :: RepMap
+  }
 
-assembled :: BundlingSetup _ _
-assembled =
-  emptySetup
-    & addBundle bundleA
-      . addFactory factoryFoo
+data BundleTyped types = BundleTyped BundleMeta (HList types)
+
+getExport :: forall t. (Typeable t) => Bundle -> Maybe t
+getExport (Bundle _ exports) = lookupT @t exports
+
+data FactorySpec
+  = FactorySpec Symbol TypeSet TypeSet
+
+newtype Factory (spec :: FactorySpec)
+  = Factory ([Bundle] -> [Bundle])
+
+class ToRepMap t where
+  toRepMap :: t -> RepMap
+
+instance ToRepMap (HList '[]) where
+  toRepMap HNil = []
+
+instance (Typeable t, ToRepMap (HList ts)) => ToRepMap (HList (t ': ts)) where
+  toRepMap (a ::: more) =
+    (typeRep (Proxy @t), unsafeCoerce a) : toRepMap more
+
+class FromRepMap t where
+  fromRepMap :: RepMap -> t
+
+instance FromRepMap (HList '[]) where
+  fromRepMap _ = HNil
+
+instance (t ~ Maybe u, Typeable u, FromRepMap (HList ts)) => FromRepMap (HList (t ': ts)) where
+  fromRepMap repMap = lookupT @u repMap ::: fromRepMap @(HList ts) repMap
+
+standalone ::
+  forall name types.
+  (ToRepMap (HList types), KnownSymbol name) =>
+  HList types ->
+  Factory ( 'FactorySpec name TS.Empty (TS.FromList types))
+standalone feats = Factory $ const [Bundle meta exports]
+ where
+  meta = symbolVal (Proxy @name) <> ".bundle"
+  exports = toRepMap feats
+
+type MapTypes :: forall k l. (k -> l) -> [k] -> [l]
+type family MapTypes f types where
+  MapTypes _ '[] = '[]
+  MapTypes f (t ': ts) = f t ': MapTypes f ts
+
+factory ::
+  forall name inputs outputs.
+  ( FromRepMap (HList (MapTypes Maybe inputs))
+  , ToRepMap (HList (MapTypes Maybe outputs))
+  ) =>
+  ([BundleTyped inputs] -> [BundleTyped outputs]) ->
+  Factory ( 'FactorySpec name (TS.FromList inputs) (TS.FromList outputs))
+factory runFactory = Factory go
+ where
+  go bundles = toBundle <$> runFactory (fromBundle <$> bundles)
+  toBundle (BundleTyped meta exports) = Bundle meta (toRepMap exports)
+  fromBundle Bundle{bundleMeta, bundleExports} = BundleTyped bundleMeta (fromRepMap bundleExports)
+
+bla =
+  factory
+    @"foo"
+    @'[Word, Char, String]
+    @'[Natural, String, [String]]
+    (build . Foldable.foldl' collect empty)
+ where
+  empty = ([], [], [], [])
+  collect (metas, ws, cs, strs) _ = _
+  build (metas, ws, cs, strs) = _
---
+++ src/Bundling/TypeSet.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE NoStarIsType #-}
+
+module Bundling.TypeSet where
+
+import Data.Kind (Type, Constraint)
+
+newtype TypeSet = TS [Type]
+
+type Empty :: TypeSet
+type Empty = 'TS '[]
+
+type Single :: Type -> TypeSet
+type Single t = 'TS '[t]
+
+type FromList :: [Type] -> TypeSet
+type FromList ts = Union Empty ('TS ts)
+
+type UnionAll :: [TypeSet] -> TypeSet
+type family UnionAll typeSets where
+  UnionAll typeSets = UnionAllInto ('TS '[]) typeSets
+
+type UnionAllInto :: TypeSet -> [TypeSet] -> TypeSet
+type family UnionAllInto into typeSets where
+  UnionAllInto into '[] = into
+  UnionAllInto into (set : more) = UnionAllInto (Union into set) more
+
+type Union :: TypeSet -> TypeSet -> TypeSet
+type family Union into from where
+  Union into ( 'TS '[]) = into
+  Union ( 'TS into) ( 'TS (t : more)) = Union ( 'TS (InsertNew t into)) ( 'TS more)
+
+type InsertNew :: Type -> [Type] -> [Type]
+type family InsertNew name into where
+  InsertNew t '[] = '[t]
+  InsertNew t (t : more) = t : more
+  InsertNew t (u : more) = u : InsertNew t more
+
+type Diff :: TypeSet -> TypeSet -> TypeSet
+type family Diff from toRemove where
+  Diff from ( 'TS '[]) = from
+  Diff ( 'TS from) ( 'TS (t : moreToRemove)) = Diff ( 'TS (RemoveFrom t from)) ( 'TS moreToRemove)
+
+type RemoveFrom :: Type -> [Type] -> [Type]
+type family RemoveFrom t from where
+  RemoveFrom t '[] = '[]
+  RemoveFrom t (t : more) = more
+  RemoveFrom t (u : more) = u : RemoveFrom t more
+
+type UnionRightDiffIsIdentity :: TypeSet -> TypeSet -> Constraint
+type UnionRightDiffIsIdentity a b = Union a (Diff a b) ~ a
+
+type UnionLeftDiffIsIdentity :: TypeSet -> TypeSet -> Constraint
+type UnionLeftDiffIsIdentity a b = Union (Diff a b) b ~ a
+
+type Elem :: Type -> TypeSet -> Bool
+type family Elem t set where
+  Elem t ('TS '[]) = 'False
+  Elem t ('TS (t ': _)) = 'True
+  Elem t ('TS (u ': more)) = Elem t ('TS more)
